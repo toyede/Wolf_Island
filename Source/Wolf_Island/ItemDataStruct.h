bc24a3e2f2988969aefd50b2307c0f10d94e82e3
ljh_6200 <86048372+ljh6200@users.noreply.github.com>
2025-09-15 23:25:40 +0900

    아이템 데이터 테이블 및 매핑 함수 제작

---
+++ Source/Wolf_Island/ItemDataStruct.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemDataStruct.generated.h"
+
+USTRUCT(BlueprintType)
+struct FItemData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Sort;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Health;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Stamina;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Water;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Hungry;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Durability;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int Overlap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* Mesh;
+};
