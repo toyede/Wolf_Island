39a0f8685d54ba4cd98fd964e2e4df1637ccf5bb
kshpt9043 <94543491+kshpt9043@users.noreply.github.com>
2026-02-22 16:11:22 +0900

    동물 아이템 드랍

--- Source/Wolf_Island/Private/Interaction/RecordActor.cpp
+++ Source/Wolf_Island/Private/Interaction/RecordActor.cpp
@@ -1,7 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Games/MainGameState.h"
+
 #include "Interaction/RecordActor.h"
+#include "Games/MainGameState.h"
 
 #include "Serialization/ObjectAndNameAsStringProxyArchive.h"
 
