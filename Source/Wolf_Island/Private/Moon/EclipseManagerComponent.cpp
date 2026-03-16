3eb86230b9ead5c87e75f702d4d6829691e0c273
Wooyong Jang <102710767+toyede@users.noreply.github.com>
2026-03-16 14:58:00 +0900

    일식 호출 함수 구현
    매일 5프로씩 증가
    한번 호출 되고 나면 그 게임내에서 더이상 호출 안됨.
    
    추가 구현해야 할 부분
    상한선 50프로 구현해야 함.
    리플리케이션 마저 구현해야 함.

---
+++ Source/Wolf_Island/Private/Moon/EclipseManagerComponent.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Moon/EclipseManagerComponent.h"
+#include "Net/UnrealNetwork.h"
+
+// Sets default values for this component's properties
+UEclipseManagerComponent::UEclipseManagerComponent()
+{
+	SetIsReplicatedByDefault(true);
+}
+
+void UEclipseManagerComponent::Server_SetEclipseOccurred_Implementation()
+{
+    bEclipseOccurred = true;
+    bEclipseTodayConfirmed = false;
+}
+
+void UEclipseManagerComponent::OnRep_EclipseTodayConfirmed()
+{
+}
+
+void UEclipseManagerComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UEclipseManagerComponent, bEclipseOccurred);
+	DOREPLIFETIME(UEclipseManagerComponent, bEclipseTodayConfirmed);
+    DOREPLIFETIME(UEclipseManagerComponent, EclipseChance);
+}
+
+void UEclipseManagerComponent::Multicast_ConfirmEclipseToday_Implementation()
+{
+    bEclipseTodayConfirmed = true;
+}
+
+void UEclipseManagerComponent::Server_OnNewDay_Implementation(int32 CurrentDay)
+{
+    // 이미 게임 중 일식이 발생했으면 스킵
+    if (bEclipseOccurred) return;
+
+    // 첫날은 0%, 매일 5%씩 증가
+    EclipseChance = FMath::Clamp(CurrentDay * 0.05f, 0.0f, 1.0f);
+    float Roll = FMath::FRand();
+
+    if (Roll < EclipseChance)
+    {
+        Multicast_ConfirmEclipseToday();
+    }
+    else
+    {
+        bEclipseTodayConfirmed = false;
+    }
+}
+
